a001b687e0413174d913034c9ddcdf104294fbea
AlanPinheiro96 <alantnt15@gmail.com>
2025-04-10 20:35:34 -0300

    Finaliza exercicio 1

--- AtividadesII/Components/Pedagio.js
+++ AtividadesII/Components/Pedagio.js
@@ -1,33 +1,49 @@
+// 2. Simulador de Pedágio
+// Crie um aplicativo onde o usuário informe:
+// ●       Tipo de veículo (Carro, Moto, Caminhão)
+// ●       Distância percorrida (em km)
+// O pedágio é cobrado com base no tipo de veículo:
+// ●       Carro: R$ 0,10 por km
+// ●       Moto: R$ 0,05 por km
+// ●       Caminhão: R$ 0,15 por km
+// O app deve exibir o total a pagar.
+// Exemplo:
+// ●       Tipo: Carro
+// ●       Distância: 200 km
+// ●       Total: R$ 20,00
+
+
 import { useState } from 'react';
 import { View, Text, TextInput, Button, ImageBackground, StyleSheet } from 'react-native';
 
-const calculoHoraExtra = () => {
-    const [salarioBase, setvalorSalario] = useState(0);
-    const [horasTrabalhadas, setTotalHora] = useState(0);
-    const [salarioFinal, setvalorFinal] = useState(0);
-    const horasNormais = 160;
-
+const pedagio = () => {
+    const [veiculo, setVeiculo] = useState('');
+    const [km, setKm] = useState(0);
+    const [valorFinal, setvalorFinal] = useState(0);
+    const valorCarro = 0.10;
+    const valorMoto = 0.05;
+    const valorCaminhao = 0.15;
+    
     return (
 
         <View style={styles.constainer}>
-                <Text style={styles.fonte}>Salario base</Text>
-                <Text style={styles.fontes}>Salário base</Text>
+                <Text style={styles.fonte}>Tipo de veiculo</Text>
+                <Text style={styles.fontes}>Tipo de veiculo</Text>
                 <TextInput
-                    value={salarioBase}
-                    onChangeText={setvalorSalario}
-                    placeholder="Valor do salario"
+                    value={veiculo}
+                    onChangeText={setVeiculo}
+                    placeholder="Informe o Tipo de veículo(carro,moto,caminhão)"
                 />
-                <Text style={styles.fontes}>Horas trabalhadas</Text>
+                <Text style={styles.fontes}>Distância em KM</Text>
                 <TextInput
-                    value={horasTrabalhadas}
-                    onChangeText={setTotalHora}
-                    placeholder="Total de horas trabalhadas"
+                    value={km}
+                    onChangeText={setKm}
+                    placeholder="Total distancia percorrida em KM/h "
                 />
                 <Button
-                    title="Calcular"
+                    title="Exibir total a pagar"
                     onPress={() => {
-                        const salario = Number(salarioBase);
-                        const horas = Number(horasTrabalhadas);
+                        const kmPorHora = Number(km);
                         const valorHora = salario / horasNormais;
 
                         if (horas <= horasNormais) {
@@ -43,7 +59,7 @@ const calculoHoraExtra = () => {
                 />
                 <Text style={styles.fontes}>Salário base: {salarioBase}</Text>
                 <Text style={styles.fontes}>Horas trabalhadas: {horasTrabalhadas}</Text>
-                <Text style={styles.fontes}>Salário final: {salarioFinal.toFixed(2)}</Text>
+                <Text style={styles.fontes}>Salário final: {valorFinal.toFixed(2)}</Text>
         </View>
 
 
@@ -71,4 +87,4 @@ const styles = StyleSheet.create({
     }
 });
 
-export default calculoHoraExtra;
+export default pedagio;
