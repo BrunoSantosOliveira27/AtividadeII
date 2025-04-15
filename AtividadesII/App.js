670572ca28c7affae47e20e383475e2690116f6b
AlanPinheiro96 <alantnt15@gmail.com>
2025-04-14 22:08:44 -0300

    Finaliza Exercicios

--- AtividadesII/App.js
+++ AtividadesII/App.js
@@ -4,12 +4,11 @@ import CalculoHoraExtra from './Components/CalculoHoraExtra'
 import Pedagio from './Components/Pedagio';
 import TarifaLuz from './Components/ContaDeLuz';
 import Tabuleiro from './Components/Tabuleiro';
-import Botao from './Components/Botao';
 
 export default function App() {
   return (
     <SafeAreaView>
-      <Tabuleiro/>
+      <CalculoHoraExtra/>
     </SafeAreaView>
   );
 }
